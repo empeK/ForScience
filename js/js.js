01039d554256f7622645b817d23df586cc0c0926
empeK <darkhides@gmail.com>
2018-01-06 17:55:19 +0200

    some fixes

--- js/js.js
+++ js/js.js
@@ -3,17 +3,14 @@ window.onscroll = nav;
 function nav() {
     var scroll = pageYOffset;
     if (scroll > 5) {
-        document.getElementById('navmain').classList.add("nav-fixed");
-        document.getElementById('coming-soon').style.opacity = '.0';
-
+        document.getElementById('coming-soon').style.opacity = '.0'
     } else {
-        document.getElementById('navmain').classList.remove("nav-fixed");
-        document.getElementById('coming-soon').style.opacity = '.6';
+        document.getElementById('coming-soon').style.opacity = '.6'
     }
     if (scroll > 300) {
-        document.getElementById('slider-section').style.opacity = '1';
+        document.getElementById('slider-section').style.opacity = '1'
 
     } else {
-        document.getElementById('slider-section').style.opacity = '.0';        
+        document.getElementById('slider-section').style.opacity = '.0'      
     }
 }
